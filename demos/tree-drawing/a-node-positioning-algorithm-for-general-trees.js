4faf94ec9e1b5b01b8b7ac1fe9fe8b93949dc6c1
Bowen7 <zwxdyx@foxmail.com>
2023-01-05 23:31:58 +0800

    walker algorithm apportion

--- demos/tree-drawing/a-node-positioning-algorithm-for-general-trees.js
+++ demos/tree-drawing/a-node-positioning-algorithm-for-general-trees.js
@@ -2,18 +2,97 @@ const isLeaf = (node) => node.children?.length === 0
 
 const hasLeftSibling = (node) => node.parent?.children.indexOf(node) > 0
 
+const hasRightSibling = (node) =>
+  node.parent.children.indexOf(node) < node.parent.children.length - 1
+
 const getLeftSibling = (node) =>
   node.parent.children[node.parent.children.indexOf(node) - 1]
+
+const getRightSibling = (node) =>
+  node.parent.children[node.parent.children.indexOf(node) + 1]
+
+const getLeftNeighbor = (node) => node.neighbor
+
+const getLeftmost = (node, level, depth) => {
+  if (level >= depth) {
+    return node
+  } else if (isLeaf(node)) {
+    return null
+  } else {
+    let rightmost = node.children[0]
+    let leftmost = getLeftmost(rightmost, level + 1, depth)
+    while (!leftmost && hasRightSibling(rightmost)) {
+      rightmost = getRightSibling(rightmost)
+      leftmost = getLeftmost(rightmost, level + 1, depth)
+    }
+    return leftmost
+  }
+}
 
 // unit length
 const WIDTH = 1
 const MIN_SEPARATION = 1
 
 export const walkerAlgorithm = (root) => {
-  const apportion = (node, level) => {}
+  const neighbors = []
+  let maxDepth = 0
+
+  const apportion = (node, level) => {
+    let leftmost = node.children[0]
+    const neighbor = getLeftNeighbor(leftmost)
+    let compareDepth = 1
+    const depthToStop = maxDepth - level
+    while (leftmost && compareDepth <= depthToStop) {
+      let leftModSum = 0
+      let rightModSum = 0
+      let ancestorLeftmost = leftmost
+      let ancestorNeighbor = neighbor
+      for (let i = 0; i < compareDepth; i++) {
+        ancestorLeftmost = ancestorLeftmost.parent
+        ancestorNeighbor = ancestorNeighbor.parent
+        rightModSum += ancestorLeftmost.modifier
+        leftModSum += ancestorNeighbor.modifier
+      }
+      let moveDistance =
+        neighbor.prelim +
+        leftModSum +
+        MIN_SEPARATION +
+        WIDTH -
+        (leftmost.prelim + rightModSum)
+      if (moveDistance > 0) {
+        let tempPtr = node
+        let leftSiblings = 0
+        while (tempPtr && tempPtr !== ancestorLeftmost) {
+          leftSiblings++
+          tempPtr = getLeftSibling(tempPtr)
+        }
+        if (tempPtr) {
+          const portion = moveDistance / leftSiblings
+          tempPtr = node
+          while (tempPtr !== ancestorLeftmost) {
+            tempPtr.prelim += moveDistance
+            tempPtr.modifier += moveDistance
+            moveDistance -= portion
+            tempPtr = getLeftSibling(tempPtr)
+          }
+        }
+      }
+
+      compareDepth++
+      if (isLeaf(leftmost)) {
+        leftmost = getLeftmost(node, 0, compareDepth)
+      } else {
+        leftmost = leftmost.children[0]
+      }
+    }
+  }
 
   const firstWalk = (node, level) => {
+    node.neighbor = neighbors[level]
+    neighbors[level] = node
     node.modifier = 0
+    maxDepth = Math.max(level, maxDepth)
+
     if (isLeaf(node)) {
       if (hasLeftSibling(node)) {
         node.prelim = getLeftSibling(node).prelim + MIN_SEPARATION + WIDTH
